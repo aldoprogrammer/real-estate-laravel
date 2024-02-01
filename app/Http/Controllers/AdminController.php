1f895c085bc5b5671dfaebee9049070af4679711
aldoprogrammer <aldobesma@gmail.com>
2024-02-01 17:43:58 +0700

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
@@ -11,6 +13,17 @@ class AdminController extends Controller
     public function AdminDashboard()
     {
         return view('admin.index');
+    }
+
+    public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
     }
 
 
--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,11 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::middleware(['auth', 'role:admin'])->group(function(){
-    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->
+    name('admin.dashboard');
+
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->
+    name('admin.logout');
 });
 
 
